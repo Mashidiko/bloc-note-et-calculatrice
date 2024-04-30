b569951b6dce2086f6dbdd594a49ed37b807fecb
mashidiko <christmashidiko@gmail.com>
2024-04-30 03:40:07 +0200

    prémier dépôt

---
+++ bloc_note.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import font as tkFont
+from tkinter import *
+from tkinter import filedialog, messagebox
+from tkinter.colorchooser import askcolor
+from tkinter import ttk  # Importez ttk pour la calculatrice
+
+# Définition de la taille de police par défaut
+current_font_size = 12
+current_font = ("Arial", current_font_size)
